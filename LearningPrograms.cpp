422cdde333a60712f2377dc6710c55bf365f4d72
Harchenko Nickolay <kolaharcenko401@gmail.com>
2023-03-07 20:28:31 +0300

    Added a new function to test how pointers work

--- LearningPrograms.cpp
+++ LearningPrograms.cpp
@@ -2,9 +2,15 @@
 
 using namespace std;
 
+int upper(int *pa){
+    (*pa)++;
+}
+
 int main(){
     int a = 5;
     int *pa = &a; // обязательно используем тот же тип указателя, что и у значения
-    cout<<"a = "<<*pa;
+    cout<<"a = "<<*pa<<"\n";
+    upper(&a);
+    cout<<"a = "<<a;
     return 0;
 }
